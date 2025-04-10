018417c96308de56505d3d94ffe339d10c809f7f
ontu001 <mdrohanurrahmanontu@gmail.com>
2025-04-10 21:55:31 +0600

    Case statemebnt added

--- class 5.sql
+++ class 5.sql
@@ -56,3 +56,78 @@ Select * from product_groups;
 
 
 
+-- Case statement 
+--Use to make logical condition starts with 'case' then at the middhe set the conditions using when and else theen finish with 'end'
+
+/*
+Categorise the price from the products table  based on the budget range,
+If the
+1. price>700 = Premium
+2. price <300 = Budget Frindly
+3. Price between 300 to 700 = Mid Range
+*/
+
+
+select
+	product_name,
+	price,
+	case
+		when price> 700 then'Premium'
+		when price between 300 and 700 then 'Mid Range'
+		when price< 300 then 'Budget Friendly'
+		else 'Undefined'
+	end as Price_Category
+from
+	products;
+
+
+
+
+
+
+-- Find out how many products are there under the 3 price category
+
+select
+	case
+		when price> 700 then'Premium'
+		when price between 300 and 700 then 'Mid Range'
+		when price< 300 then 'Budget Friendly'
+		else 'Undefined'
+	end as Price_Category,
+	count
+		(product_name) as Product_Count
+from
+	products
+group by
+	1       -- using 1 means I want to group the result base on !st column,(Best PRactice). We can also use "Price_Category" instade of 1
+order by
+	Product_Count asc;
+
+
+
+
+-- Add 15%, 20% and 10% discount based on product_group
+select
+    product_name,
+    price,
+    group_id,
+    case
+        when group_id = 1 then round(price * 0.85, 2)
+        when group_id = 2 then round(price * 0.80, 2)
+        when group_id = 3 then round(price * 0.90, 2)
+        else price
+    end as Discount_Price,
+    case -- to show dicounted amount
+        when group_id = 1 then round(price - (price * 0.85), 2)
+        when group_id = 2 then round(price - (price * 0.80), 2)
+        when group_id = 3 then round(price - (price * 0.90), 2)
+        else 0
+    end as Discount_Amount,
+    case -- to show dicount percentage with the % symbol
+        when group_id = 1 then cast(round((1 - 0.85) * 100, 0) as varchar) || '%'
+        when group_id = 2 then cast(round((1 - 0.80) * 100, 0) as varchar) || '%'
+        when group_id = 3 then cast(round((1 - 0.90) * 100, 0) as varchar) || '%'
+        else '0%'
+    end as Discount_Percentage
+from
+    products;
